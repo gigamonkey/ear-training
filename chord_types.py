76ebb3131a778f0c6a9189c5ce7099dec9976f89
Peter Seibel <peter@gigamonkeys.com>
2020-12-03 13:43:40 -0800

    Abstracting quiz structure a bit.

--- chord_types.py
+++ chord_types.py
@@ -25,9 +25,21 @@ initial_button_color = (127, 127, 255)
 wrong_button_color = (64, 64, 255)
 
 
+class Question:
+    def __init__(self, name, pattern):
+        self.label = name
+        self.chord = ChordType(name, pattern)
+
+    def play(self, midi_out):
+        play(midi_out, self.chord.render_chord(60, 120))
+
+    def hint(self, midi_out):
+        play(midi_out, self.chord.render_arpeggio_and_chord(60, 120))
+
+
 class Button:
-    def __init__(self, label, pos, size, is_wrong):
-        self.label = label
+    def __init__(self, question, pos, size, is_wrong):
+        self.question = question
         self.rect = pygame.Rect(pos, size)
         self.is_wrong = is_wrong
 
@@ -38,13 +50,16 @@ class Button:
             initial_button_color if not self.is_wrong else wrong_button_color,
             pygame.Rect(0, 0, self.rect.width, self.rect.height),
         )
-        text, text_rect = font.render(self.label, (0, 0, 0))
+        text, text_rect = font.render(self.question.label, (0, 0, 0))
 
         x = (self.rect.width - text_rect.width) / 2
         y = (self.rect.height - text_rect.height) / 2
 
         surface.blit(text, (x, y))
         screen.blit(surface, (self.rect.x, self.rect.y))
+
+    def is_hit(self, pos):
+        return self.rect.collidepoint(pos)
 
 
 def is_quit(e):
@@ -57,47 +72,44 @@ def is_replay(e):
     return e.type == pygame.KEYDOWN and e.key == pygame.K_SPACE
 
 
-def is_replay_with_arepeggio(e):
-    return e.type == pygame.KEYDOWN and e.key == pygame.K_a
+def is_replay_with_hint(e):
+    return e.type == pygame.KEYDOWN and e.key in {pygame.K_a, pygame.K_h}
 
 
-def render_buttons(surface, labels, rect, font, wrong, gap=5):
+def render_buttons(surface, quiz, rect, font, wrong, gap=5):
 
-    h = ((rect.height + gap) / len(labels)) - gap
+    h = ((rect.height + gap) / len(quiz)) - gap
 
-    buttons = [
-        Button(label, (0, i * (h + gap)), (rect.width, h), label in wrong)
-        for i, label in enumerate(labels)
-    ]
-    for b in buttons:
+    def make_button(q, i):
+        b = Button(q, (0, i * (h + gap)), (rect.width, h), q.label in wrong)
         b.render(surface, font)
+        return b
 
-    return buttons
+    return [make_button(q, i) for i, q in enumerate(quiz)]
 
 
-def get_answer(buttons, replay, replay_with_arpeggio):
+def get_answer(buttons, question, midi_out):
     while True:
         event = pygame.event.wait()
         if is_quit(event):
             return False, None
         elif is_replay(event):
-            replay()
-        elif is_replay_with_arepeggio(event):
-            replay_with_arpeggio()
+            question.play(midi_out)
+        elif is_replay_with_hint(event):
+            question.hint(midi_out)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             for b in buttons:
-                if b.rect.collidepoint(event.pos):
-                    return True, b.label
+                if b.is_hit(event.pos):
+                    return True, b.question
 
 
 def run():
 
-    questions = [ChordType(name, pattern) for name, pattern in chord_types.items()]
+    questions = [Question(name, pattern) for name, pattern in chord_types.items()]
 
     pygame.init()
     pygame.midi.init()
 
-    # font = pygame.freetype.SysFont(pygame.freetype.get_default_font(), 32)
     font = pygame.freetype.SysFont("helveticaneue", 32)
 
     pygame.display.set_caption("Chord types")
@@ -106,8 +118,6 @@ def run():
 
     background = pygame.Surface(size)
     background.fill(pygame.Color("#dddddd"))
-
-    pygame.time.Clock()
 
     pygame.event.set_blocked(pygame.MOUSEMOTION)
 
@@ -124,36 +134,32 @@ def run():
         while running:
 
             if not wrong:
-                # New question
-                to_play = random.choice(questions)
+                question = random.choice(questions)
                 quiz = questions
-                first_midi = to_play.render_chord(60, 120)
-                second_midi = to_play.render_arpeggio_and_chord(60, 120)
-
-                play_question = lambda: play(midi_out, first_midi)
 
             # Draw the screen with the buttons.
             screen.blit(background, (0, 0))
             buttons = render_buttons(
-                screen, [str(p) for p in quiz], pygame.Rect(0, 0, 300, 500), font, wrong
+                screen, quiz, pygame.Rect(0, 0, 300, 500), font, wrong
             )
             pygame.display.update()
 
             # Play the progression
             pygame.event.clear()
-            play_question()
+            question.play(midi_out)
 
             # Wait for events until we get a button click; check the answer.
-            running, answer = get_answer(
-                buttons, play_question, lambda: play(midi_out, second_midi)
-            )
+            running, choice = get_answer(buttons, question, midi_out)
             if running:
-                if answer == str(to_play):
+                if choice == question:
                     play(midi_out, sequence(chirp, 120))
                     wrong = set()
                 else:
-                    play(midi_out, sequence(blat, 120))
-                    wrong.add(answer)
+                    if choice.label in wrong:
+                        choice.play(midi_out)
+                    else:
+                        play(midi_out, sequence(blat, 120))
+                        wrong.add(choice.label)
 
     finally:
         pygame.midi.quit()
