e025b97d05498d2a45ed19c5b7b36960765d2a8a
Peter Seibel <peter@gigamonkeys.com>
2020-12-12 15:49:12 -0800

    Break out trivial piano app from computer keyboard code.

--- keyboard.py
+++ keyboard.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
-"Trivial computer keyboard based piano keyboard."
+"Mapping from keyboard scancodes to chromatic and diatonic notes."
 
 import pygame
-import pygame.midi
 
-from app import is_quit
 from music import Scale
 
 #
@@ -69,46 +67,3 @@ def get_note(e):
 def get_diatonic_note(e):
     if e.type in {pygame.KEYDOWN, pygame.KEYUP}:
         return diatonic_key_mapping.get(e.scancode)
-
-
-def main():
-
-    start_note = 60
-
-    pygame.init()
-    pygame.midi.init()
-
-    port = pygame.midi.get_default_output_id()
-    midi_out = pygame.midi.Output(port, 0)
-
-    try:
-        midi_out.set_instrument(0)
-
-        on_notes = set()
-
-        while True:
-            e = pygame.event.wait()
-
-            if is_quit(e):
-                break
-            elif (note := get_note(e)) is not None:
-                note += start_note
-                if e.type == pygame.KEYDOWN:
-                    if note not in on_notes:
-                        print(note)
-                        midi_out.note_on(note, 127)
-                        on_notes.add(note)
-                    else:
-                        print(e.scancode)
-                elif e.type == pygame.KEYUP:
-                    if note in on_notes:
-                        midi_out.note_off(note, 0)
-                        on_notes.remove(note)
-
-    finally:
-        pygame.midi.quit()
-
-
-if __name__ == "__main__":
-
-    main()
---
+++ piano.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"Trivial computer keyboard based piano keyboard."
+
+import pygame
+import pygame.midi
+
+import keyboard
+from app import is_quit
+
+if __name__ == "__main__":
+
+    start_note = 60
+
+    pygame.init()
+    pygame.midi.init()
+
+    port = pygame.midi.get_default_output_id()
+    midi_out = pygame.midi.Output(port, 0)
+
+    try:
+        midi_out.set_instrument(0)
+
+        on_notes = set()
+
+        while True:
+            e = pygame.event.wait()
+
+            if is_quit(e):
+                break
+            elif (note := keyboard.get_note(e)) is not None:
+                note += start_note
+                if e.type == pygame.KEYDOWN:
+                    if note not in on_notes:
+                        print(note)
+                        midi_out.note_on(note, 127)
+                        on_notes.add(note)
+                    else:
+                        print(e.scancode)
+                elif e.type == pygame.KEYUP:
+                    if note in on_notes:
+                        midi_out.note_off(note, 0)
+                        on_notes.remove(note)
+
+    finally:
+        pygame.midi.quit()
