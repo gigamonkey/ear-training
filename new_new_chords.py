7cef3b30fc7e5dc8787517fa2408b572158e733f
Peter Seibel <peter@gigamonkeys.com>
2021-09-09 15:25:28 -0700

    Some old work on yet another way to present questions. Probably about to become obsolete.

---
+++ new_new_chords.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+"""
+Quiz of chords using set scheduler that weights questions that
+you've gotten wrong more heavily and adds new questions when you have
+gotten all current questions more right than wrong.
+"""
+
+import dataclasses
+from dataclasses import dataclass
+from typing import Tuple
+
+from app import Quiz
+from midi import play
+from music import chord
+from music import chord_types
+from music import melody
+from scheduler import SetQuestionScheduler
+
+
+@dataclass(frozen=True)
+class ChordQuestion:
+
+    chord_pattern: Tuple[int]
+    root_note: int
+
+    def for_type(self, chord_pattern):
+        return dataclasses.replace(self, chord_pattern=chord_pattern)
+
+    @property
+    def label(self):
+        return chord_types[self.chord_pattern]
+
+    @property
+    def notes(self):
+        return [self.root_note + i for i in self.chord_pattern]
+
+    def options(self, in_play):
+        return [self.for_type(s) for s in sorted({q.chord_pattern for q in in_play})]
+
+    def play(self, midi_out):
+        play(midi_out, chord(self.notes).render(0, 120))
+
+    def hint(self, midi_out):
+        play(midi_out, melody(self.notes).render(0, 120))
+
+    def after_correct(self, midi_out):
+        pass
+
+
+class ChordsQuiz(Quiz):
+    def __init__(self, name, kinds):
+        super().__init__(name)
+
+        def question_set(root):
+            return [ChordQuestion(pattern, root) for pattern in kinds]
+
+        fourths = [n % 12 for n in range(0, (5 * 12), 5)]
+        octaves = [0] + [i * s for i in range(1, 5) for s in (1, -1)]
+        roots = [(r + 60) + (o * 12) for o in octaves for r in fourths]
+        question_sets = [question_set(r) for r in roots]
+        self.scheduler = SetQuestionScheduler(question_sets, 0.5, 1.01, 5)
+
+    def make_questions(self, _):
+        expected = self.scheduler.draw()
+        return expected, self.scheduler.options(expected)
+
+    def update(self, choice, question):
+        self.scheduler.update(choice, question)
+
+
+if __name__ == "__main__":
+
+    seventh_chords = [
+        (0, 4, 7),  # "Major",
+        (0, 3, 7),  # "Minor",
+        (0, 3, 6),  # "Diminished",
+        (0, 4, 8),  # "Augmented",
+        # (0, 3, 6, 10),
+        # (0, 3, 6, 9),
+        # (0, 3, 7, 10),
+        # (0, 4, 7, 10),
+        # (0, 4, 7, 11),
+    ]
+
+    ChordsQuiz("Chords", seventh_chords).run()
--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,6 @@
 import random
 from collections import defaultdict
+from dataclasses import dataclass
 
 
 class QuestionScheduler:
@@ -29,7 +30,7 @@ class QuestionScheduler:
         for a, b in sorted(
             zip(weights, pop), key=lambda x: (x[0], x[1].label), reverse=True
         ):
-            print(f"{a:.4f} -> {b.label} {b}")
+            print(f"{a:.4f} -> {b.label}")
 
         return random.choices(pop, weights, k=1)[0]
 
@@ -55,5 +56,96 @@ class QuestionScheduler:
             print(f"Adding {q.label} {q}")
             self.scores[q] = 0.0
             return q
+        except StopIteration:
+            return None
+
+
+@dataclass
+class QuestionData:
+
+    score: float = 0.0
+    last_asked: int = 0
+
+    def weight(self, s):
+        base_weight = s.limit - self.score
+        age = s.questions_asked - self.last_asked
+        age_adjustment = s.age_weighting ** age
+        weight = base_weight * age_adjustment
+        return weight
+
+    def stats(self, s):
+        base_weight = s.limit - self.score
+        age = s.questions_asked - self.last_asked
+        age_adjustment = s.age_weighting ** age
+        weight = base_weight * age_adjustment
+        return f"weight {weight}. score: {self.score}; base_weight: {base_weight}; age: {age}; age_adjustment: {age_adjustment}"
+
+    def ok(self, threshold):
+        return self.score > threshold
+
+
+class SetQuestionScheduler:
+
+    """
+    Like QuestionScheduler but adds questions in sets rather than one
+    at a time.
+    """
+
+    def __init__(self, question_sets, score_decay, age_weighting, correct_required):
+        self.question_sets = iter(question_sets)
+        self.questions = dict()
+        self.score_decay = score_decay
+        self.age_weighting = age_weighting
+        self.limit = 1 / (1 - score_decay)
+        self.threshold = self.limit - (score_decay ** (correct_required - 1))
+        self.questions_asked = 0
+
+    def draw(self):
+
+        if self.needs_new_questions():
+            self.add_questions()
+
+        qs = list(self.questions.keys())
+        weights = [self.questions[q].weight(self) for q in qs]
+
+        print("")
+        print(f"{len(qs)} current questions.")
+        for a, b in sorted(
+            zip(weights, qs), key=lambda x: (x[0], x[1].label), reverse=True
+        ):
+            qd = self.questions[b]
+            print(f"{a:.4f} -> {b.label} {qd.stats(self)}")
+
+        self.questions_asked += 1
+        q = random.choices(qs, weights, k=1)[0]
+        self.questions[q].last_asked = self.questions_asked
+        return q
+
+    def options(self, expected):
+        return expected.options(self.questions.keys())
+
+    def update(self, got, expected):
+        if got == expected:
+            self.questions[got].score *= self.score_decay
+            self.questions[got].score += 1
+        else:
+            self.questions[got].score *= self.score_decay
+            self.questions[got].score -= 1
+            self.questions[expected].score *= self.score_decay
+            self.questions[expected].score -= 1
+
+    def needs_new_questions(self):
+        """
+        When all the current questions' scores are above our threshold,
+        then we add in the next set of questions.
+        """
+        return all(q.ok(self.threshold) for q in self.questions.values())
+
+    def add_questions(self):
+        try:
+            qs = next(self.question_sets)
+            for q in qs:
+                print(f"Adding {q.label} {q}")
+                self.questions[q] = QuestionData()
         except StopIteration:
             return None
