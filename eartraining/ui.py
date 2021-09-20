372ce047d8ee9599f580e08ecef83ea9be8b1bf4
Peter Seibel <peter@gigamonkeys.com>
2021-09-20 16:45:30 -0700

    More UI elements moved.

--- eartraining/ui.py
+++ eartraining/ui.py
@@ -17,6 +17,14 @@ wrong_button_color = (64, 64, 255)
 status_color = (16 * 10, 16 * 10, 255)
 
 
+def is_mouse_event(e):
+    return e.type in {pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP, pygame.MOUSEMOTION}
+
+
+def is_key_event(e):
+    return e.type in {pygame.KEYDOWN, pygame.KEYUP}
+
+
 class Button:
     def __init__(self, question, pos, size, is_wrong):
         self.question = question
@@ -98,3 +106,184 @@ def render_buttons(surface, quiz, rect, font, wrong, gap=5):
         return b
 
     return [make_button(q, i) for i, q in enumerate(quiz)]
+
+
+class ChromaticKeyboard:
+
+    "Abstracted one-octave piano keyboard widget."
+
+    keys = range(12)
+
+    white_keys = [0, 2, 4, 5, 7, 9, 11]
+
+    number_keys = [
+        pygame.K_1,
+        pygame.K_2,
+        pygame.K_3,
+        pygame.K_4,
+        pygame.K_5,
+        pygame.K_6,
+        pygame.K_7,
+    ]
+
+    def __init__(self, labels, rect, font, gap):
+        self.rect = rect
+        self.font = font
+        self.gap = gap
+
+        max_width = ((rect.width + gap) / len(ChromaticKeyboard.white_keys)) - gap
+        max_height = ((rect.height + gap) / 2) - gap
+
+        size = (min(max_width, max_height),) * 2
+        self.keys = [
+            Key(k, labels[k], self.key_rect(k, size), self.font)
+            for k in ChromaticKeyboard.keys
+        ]
+
+    @classmethod
+    def preferred_size(self, key_size, gap):
+        width = (key_size * len(ChromaticKeyboard.white_keys)) + (gap * 6)
+        height = (key_size * 2) + gap
+        return (width, height)
+
+    def key_rect(self, note, size):
+        return pygame.Rect(self.key_pos(note, size), size)
+
+    def key_pos(self, note, size):
+        if note in ChromaticKeyboard.white_keys:
+            i = ChromaticKeyboard.white_keys.index(note)
+            x = self.rect.x + (i * (size[0] + self.gap))
+            y = self.rect.y + size[1] + self.gap
+            return (x, y)
+        else:
+            x, y = self.key_pos(note - 1, size)
+            return (x + size[0] / 2, y - (size[1] + self.gap))
+
+    def draw(self, parent):
+        for k in self.keys:
+            k.draw(parent)
+
+    def handle_event(self, e, ui):
+        key = None
+
+        if is_mouse_event(e):
+            for k in self.keys:
+                if k.rect.collidepoint(e.pos):
+                    key = k
+                    break
+
+        elif (note := eartraining.keyboard.get_note(e)) is not None:
+            key = self.keys[note % 12]
+
+        elif is_key_event(e) and e.key in ChromaticKeyboard.number_keys:
+            i = Scale.major[ChromaticKeyboard.number_keys.index(e.key)]
+            key = self.keys[i]
+
+        if key is not None:
+            if e.type in {pygame.MOUSEBUTTONDOWN, pygame.KEYDOWN}:
+                key.lowlight()
+                ui.fire_key_played(key)
+            elif e.type in {pygame.MOUSEBUTTONUP, pygame.KEYUP}:
+                key.unlight()
+                ui.fire_key_released(key)
+
+
+class DiatonicKeyboard:
+
+    "Abstracted one-octave diatonic keyboard widget."
+
+    keys = range(7)
+
+    number_keys = [
+        pygame.K_1,
+        pygame.K_2,
+        pygame.K_3,
+        pygame.K_4,
+        pygame.K_5,
+        pygame.K_6,
+        pygame.K_7,
+    ]
+
+    def __init__(self, labels, rect, font, gap):
+        self.rect = rect
+        self.font = font
+        self.gap = gap
+
+        max_width = ((rect.width + gap) / len(DiatonicKeyboard.keys)) - gap
+        max_height = rect.height
+
+        size = (min(max_width, max_height),) * 2
+        self.keys = [
+            Key(k, labels[k], self.key_rect(k, size), self.font)
+            for k in DiatonicKeyboard.keys
+        ]
+
+    @classmethod
+    def preferred_size(self, key_size, gap):
+        width = (key_size * len(DiatonicKeyboard.keys)) + (gap * 6)
+        height = key_size
+        return (width, height)
+
+    def key_rect(self, note, size):
+        return pygame.Rect(self.key_pos(note, size), size)
+
+    def key_pos(self, note, size):
+        x = self.rect.x + (note * (size[0] + self.gap))
+        return (x, self.rect.y)
+
+    def draw(self, parent):
+        for k in self.keys:
+            k.draw(parent)
+
+    def handle_event(self, e, ui):
+        key = None
+
+        if is_mouse_event(e):
+            for k in self.keys:
+                if k.rect.collidepoint(e.pos):
+                    key = k
+                    break
+
+        elif (note := eartraining.keyboard.get_diatonic_note(e)) is not None:
+            key = self.keys[note % 7]
+
+        elif is_key_event(e) and e.key in DiatonicKeyboard.number_keys:
+            key = self.keys[DiatonicKeyboard.number_keys.index(e.key)]
+
+        if key is not None:
+            if e.type in {pygame.MOUSEBUTTONDOWN, pygame.KEYDOWN}:
+                key.lowlight()
+                ui.fire_key_played(key)
+            elif e.type in {pygame.MOUSEBUTTONUP, pygame.KEYUP}:
+                key.unlight()
+                ui.fire_key_released(key)
+
+
+class Key:
+    def __init__(self, note, label, rect, font):
+        self.note = note
+        self.label = label
+        self.rect = rect
+        self.font = font
+        self.color = (127, 127, 255)
+
+    def draw(self, parent):
+
+        surface = pygame.Surface(self.rect.size)
+        pygame.draw.rect(surface, self.color, pygame.Rect((0, 0), self.rect.size))
+
+        text, text_rect = self.font.render(self.label, (0, 0, 0))
+        x = (self.rect.size[0] - text_rect.width) / 2
+        y = (self.rect.size[1] - text_rect.height) / 2
+        surface.blit(text, (x, y))
+
+        parent.blit(surface, self.rect.topleft)
+
+    def unlight(self):
+        self.color = (127, 127, 255)
+
+    def highlight(self):
+        self.color = (156, 156, 255)
+
+    def lowlight(self):
+        self.color = (64, 64, 255)
